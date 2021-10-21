d995234eee81bfa39d70fdbb7016e534ef6c742a
Denis Vasilik <contact@denisvasilik.com>
2021-10-21 12:25:12 +0200

    Create PyPi package

--- setup.py
+++ setup.py
@@ -30,33 +30,43 @@ setuptools.setup(
     dependency_links=[],
     package_dir={},
     package_data={},
-    data_files=[
-        ("wasm-fpga-memory/package", ["package/component.xml"]),
-        ("wasm-fpga-memory/package/bd", ["package/bd/bd.tcl"]),
-        ("wasm-fpga-memory/package/xgui", ["package/xgui/wasm_fpga_memory_v1_0.tcl"]),
-        (
-            "wasm-fpga-memory/ip/WasmFpgaMemoryRam",
-            ["ip/WasmFpgaMemoryRam/WasmFpgaMemoryRam.xci"],
-        ),
-        (
-            "wasm-fpga-memory/src",
-            [
-                "src/WasmFpgaMemory.vhd",
-                "src/WasmFpgaMemoryPackage.vhd",
-            ],
-        ),
-        (
-            "wasm-fpga-memory/tb",
-            [
-                "tb/tb_FileIo.vhd",
-                "tb/tb_pkg_helper.vhd",
-                "tb/tb_pkg.vhd",
-                "tb/tb_std_logic_1164_additions.vhd",
-                "tb/tb_Types.vhd",
-                "tb/tb_WasmFpgaMemory.vhd",
-            ],
-        ),
-        ("wasm-fpga-memory", ["CHANGELOG.md", "AUTHORS", "LICENSE"]),
+    data_files=[(
+        "wasm-fpga-memory/package", [
+            "package/component.xml"
+        ]),(
+        "wasm-fpga-memory/package/bd", [
+            "package/bd/bd.tcl"
+        ]),(
+        "wasm-fpga-memory/package/xgui", [
+            "package/xgui/wasm_fpga_memory_v1_0.tcl"
+        ]),(
+        "wasm-fpga-memory/ip/WasmFpgaMemoryRam", [
+            "ip/WasmFpgaMemoryRam/WasmFpgaMemoryRam.xci"
+        ]),(
+        "wasm-fpga-memory/resources", [
+            "resources/debug.coe",
+        ]),(
+        "wasm-fpga-memory/src", [
+            "src/WasmFpgaMemory.vhd",
+            "src/WasmFpgaMemoryPackage.vhd",
+        ]),(
+        "wasm-fpga-memory/tb", [
+            "tb/tb_FileIo.vhd",
+            "tb/tb_pkg_helper.vhd",
+            "tb/tb_pkg.vhd",
+            "tb/tb_std_logic_1164_additions.vhd",
+            "tb/tb_Types.vhd",
+            "tb/tb_WasmFpgaMemory.vhd",
+        ]),(
+        'wasm-fpga-memory/simstm', [
+            'simstm/Defines.stm',
+            'simstm/WasmFpgaMemory.stm',
+        ]),(
+            "wasm-fpga-memory", [
+                "CHANGELOG.md",
+                "AUTHORS",
+                "LICENSE"
+        ])
     ],
     setup_requires=[],
     install_requires=[],
